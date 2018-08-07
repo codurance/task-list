0baad48d2ef9dd9f911d40da9d8033e019ad2026
xstrengthofonex <xstrengthofonex@gmail.com>
2018-08-07 21:43:28 +0900

    Update console.py with additional types

--- python/task_list/console.py
+++ python/task_list/console.py
@@ -6,7 +6,7 @@ class Console:
         self.input_reader = input_reader
         self.output_writer = output_writer
 
-    def print(self, string: Optional="", end: str="\n", flush=True) -> None:
+    def print(self, string: Optional[str]="", end: str="\n", flush: bool=True) -> None:
         self.output_writer.write(string + end)
         if flush:
             self.output_writer.flush()
