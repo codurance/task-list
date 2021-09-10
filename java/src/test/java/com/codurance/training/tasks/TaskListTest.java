ac4071ecaed4bfccfb0e4023cc2528834b986acf
Valentin Alvès <valentin.alves.partner@decathlon.com>
2021-09-10 16:00:12 +0200

    added coverage for show and check-uncheck commands

--- java/src/test/java/com/codurance/training/tasks/TaskListTest.java
+++ java/src/test/java/com/codurance/training/tasks/TaskListTest.java
@@ -1,8 +1,10 @@
 package com.codurance.training.tasks;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -12,13 +14,18 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class TaskListTest {
-    private final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    @Mock
+    private BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-    private final PrintWriter out = new PrintWriter(System.out);
+    @Mock
+    private PrintWriter out;
 
-    private final TaskList taskList = new TaskList(in, out);
+    @InjectMocks
+    private TaskList taskList;
 
     @DisplayName("Add command")
     @Nested
@@ -50,6 +57,79 @@ class TaskListTest {
                     () -> assertThat(assignments).hasSize(1),
                     () -> assertThat(assignments.get(0).getDescription()).isEqualTo("english class"));
         }
+
+        @Test
+        public void showError_whenProjectDoesNotExists() {
+            //Given
+            taskList.tasks.put("assignments", new ArrayList<>());
+            String projectCreationCommand = "add task wrongProject english class";
+
+            //When
+            taskList.execute(projectCreationCommand);
+
+            //Then
+            List<Task> assignments = taskList.tasks.get("assignments");
+            verify(out).printf("Could not find a project with the name \"%s\".",
+                    "wrongProject");
+        }
     }
+
+    @DisplayName("Show command")
+    @Nested
+    class showCommand {
+        @Test
+        public void showProjectsAndAssignments() {
+            //Given
+            taskList.tasks.put("assignments", List.of(new Task(1L, "description", false)));
+            String projectCreationCommand = "show";
+
+            //When
+            taskList.execute(projectCreationCommand);
+
+            //Then
+            verify(out).println("assignments");
+            verify(out).printf(
+                    "    [%c] %d: %s%n",
+                    ' ',
+                    1L,
+                    "description"
+            );;
+        }
+    }
+
+    @DisplayName("Check and uncheck tasks")
+    @Nested
+    class checkCommand {
+
+        @Test
+        public void checkTaskAsDone() {
+            //Given
+            taskList.tasks.put("test", List.of(new Task(1L, "description", false)));
+
+            String projectCreationCommand = "check 1";
+
+            //When
+            taskList.execute(projectCreationCommand);
+
+            //Then
+            assertThat(taskList.tasks.get("test").get(0).isDone()).isTrue();
+        }
+
+        @Test
+        public void unCheckTask() {
+            //Given
+            taskList.tasks.put("test", List.of(new Task(1L, "description", true)));
+
+            String projectCreationCommand = "uncheck 1";
+
+            //When
+            taskList.execute(projectCreationCommand);
+
+            //Then
+            assertThat(taskList.tasks.get("test").get(0).isDone()).isFalse();
+        }
+    }
+
+
 
 }
