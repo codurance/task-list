e963308db8bee776045f4577ea80814f51bf01d6
abhigyanzemoso <abhigyan.pandey@zemosolabs.com>
2023-04-11 13:23:33 +0530

    commenting-old-code

--- java/src/main/java/com/codurance/training/tasks/TaskList.java
+++ java/src/main/java/com/codurance/training/tasks/TaskList.java
@@ -1,30 +1,33 @@
-package com.codurance.training.tasks;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
-public final class TaskList {
-    private static final String QUIT = "quit";
-
-    private final Map<String, List<Task>> tasks = new LinkedHashMap<>();
-    private final BufferedReader in;
-    private final PrintWriter out;
-
-    public TaskList(BufferedReader reader, PrintWriter writer) {
-        this.in = reader;
-        this.out = writer;
-    }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-        new TaskList(in,out);
-    }
-
+//package com.codurance.training.tasks;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.io.PrintWriter;
+//import java.util.ArrayList;
+//import java.util.LinkedHashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//public final class TaskList implements Runnable {
+//    private static final String QUIT = "quit";
+//
+//    private final Map<String, List<Task>> tasks = new LinkedHashMap<>();
+//    private final BufferedReader in;
+//    private final PrintWriter out;
+//
+//    private long lastId = 0;
+//
+//    public static void main(String[] args) throws Exception {
+//        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//        PrintWriter out = new PrintWriter(System.out);
+//        new TaskList(in, out).run();
+//    }
+//
+//    public TaskList(BufferedReader reader, PrintWriter writer) {
+//        this.in = reader;
+//        this.out = writer;
+//    }
 //
 //    public void run() {
 //        while (true) {
@@ -143,4 +146,4 @@ public final class TaskList {
 //    private long nextId() {
 //        return ++lastId;
 //    }
-}
+//}
