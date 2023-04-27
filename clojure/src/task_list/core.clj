e81c8296bdd4b0435827243780bdfb35537a9f0e
Brandon Olivier <brandon@brandonolivier.com>
2023-04-27 14:55:05 -0500

    Initial working clojure version

--- clojure/src/task_list/core.clj
+++ clojure/src/task_list/core.clj
@@ -1,4 +1,78 @@
-(ns task-list.core)
+(ns task-list.core
+  (:require
+   [clojure.walk :as walk]
+   [clojure.string :as str]))
 
 (defn create-task-list []
-  {:tasks {}})
+  {})
+
+(def id-seq (atom (range)))
+
+(defn next-id []
+  (swap! id-seq rest)
+  (first @id-seq))
+
+(defn create-task [description]
+  {:id (next-id)
+   :description description})
+
+
+(defn add-task [task-list project description]
+  (let [task (create-task description)]
+    (assoc task-list project (conj (get task-list project) task))))
+
+(defn add-project [task-list project-name]
+  (assoc task-list project-name []))
+
+(defn add [task-list command-line]
+  (let [[subcommand args] (str/split command-line #" " 2)]
+    (if (= subcommand "project")
+      (add-project task-list args)
+      (when (= subcommand "task")
+        (let [[project description] (str/split args #" " 2)]
+          (add-task task-list project description))))))
+
+(defn show [task-list]
+  (doseq [[project tasks] task-list]
+    (println project)
+    (doseq [task tasks]
+      (println (str "  [" (if (:done task) "x" " ") "] " (:id task) ": " (:description task))))
+    (println "")))
+
+(defn set-done [task-list id value]
+  (let [proj-name (atom nil)
+        idx (atom nil)]
+    (loop [tasks (into [] task-list)]
+      (let [[project project-tasks] (first tasks)]
+        (doall (map-indexed (fn [i task]
+                              (when (= id (:id task))
+                                (reset! proj-name project)
+                                (reset! idx i)))
+                            project-tasks))
+
+        (if @proj-name
+          (update-in task-list [@proj-name @idx :done] (fn [_] value))
+          (if (not (empty? (rest tasks)))
+            (recur (rest tasks))
+            task-list))))))
+
+(defn check [task-list id]
+  (set-done task-list id true))
+
+(defn uncheck [task-list id]
+  (set-done task-list id false))
+
+(defn execute [task-list command-line]
+  (let [[command args] (str/split command-line #" " 2)]
+    (case command
+      "show" (do (show task-list)
+                 task-list)
+      "add" (add task-list args)
+      "check" (check task-list (Integer/parseInt args)))))
+
+(def task-list (-> (create-task-list)
+                   (add-project "secrets")
+                   (add-task "secrets" "Eat more donuts.")
+                   (add-task "secrets" "Destroy all humans.")
+                   (check 2)
+                   (uncheck 2)))
--- clojure/test/task_list/core_test.clj
+++ clojure/test/task_list/core_test.clj
@@ -5,19 +5,53 @@
 
 (declare execute)
 
+(defn reset-id-seq []
+  (reset! sut/id-seq (range)))
+
 (deftest application-test
+  (reset-id-seq)
   (let [task-list (sut/create-task-list)
         output (with-out-str
-                 (execute "show")
-                 (execute "add project secrets")
-                 (execute "add task secrets Eat more donuts.")
-                 (execute "add task secrets Destroy all humans.")
-                 (execute "show"))]
-    (is (= output
-           "secrets
-  [ ] 1: Eat more donuts.
-  [ ] 2: Destroy all humans
-"))))
+                 (-> (sut/create-task-list)
+                     (sut/execute "show")
+                     (sut/execute "add project secrets")
+                     (sut/execute "add task secrets Eat more donuts.")
+                     (sut/execute "add task secrets Destroy all humans.")
+                     (sut/execute "show")
 
-(defn execute [command]
-  (println command))
+
+
+                     (sut/execute "add project training")
+                     (sut/execute "add task training Four Elements of Simple Design")
+                     (sut/execute "add task training SOLID")
+                     (sut/execute "add task training Coupling and Cohesion")
+                     (sut/execute "add task training Primitive Obsession")
+                     (sut/execute "add task training Outside-In TDD")
+                     (sut/execute "add task training Interaction-Driven Design")
+
+                     (sut/execute "check 1")
+                     (sut/execute "check 3")
+                     (sut/execute "check 5")
+                     (sut/execute "check 6")
+                     (sut/execute "show")))]
+    (is (= "secrets
+  [ ] 1: Eat more donuts.
+  [ ] 2: Destroy all humans.
+
+secrets
+  [x] 1: Eat more donuts.
+  [ ] 2: Destroy all humans.
+
+training
+  [x] 3: Four Elements of Simple Design
+  [ ] 4: SOLID
+  [x] 5: Coupling and Cohesion
+  [x] 6: Primitive Obsession
+  [ ] 7: Outside-In TDD
+  [ ] 8: Interaction-Driven Design
+
+"
+           output))))
+
+(defn execute [task-list command]
+  (sut/execute task-list command))
